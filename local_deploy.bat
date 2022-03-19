d28e751ae68ac1a1b948bc18b0ad3d18ab6d96de
Yu Wang <yuwan@microsoft.com>
2022-03-19 10:11:35 -0700

    fix: fix local_deploy.bat

--- local_deploy.bat
+++ local_deploy.bat
@@ -1,4 +1,4 @@
 mkdir %1\.obsidian\plugins\obsidian-add-code-language\
 copy main.js %1\.obsidian\plugins\obsidian-add-code-language\
 copy manifest.json %1\.obsidian\plugins\obsidian-add-code-language\
-@REM copy styles.css %1\plugins\obsidian-add-code-language\
+copy languages.txt %1\.obsidian\plugins\obsidian-add-code-language\
